8de837d26f83a0a2b415d5b2a2c85f55e9651bf6
butterr12 <kechan1@up.edu.ph>
2025-05-25 05:30:02 +0800

    additional configurations

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  
+    allow_origins=[
+        "https://climate-sentiment.vercel.app/", 
+        "http://localhost:5173",  
+    ],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
